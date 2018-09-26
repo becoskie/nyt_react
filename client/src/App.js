56659781dae0a968e03e704e6adc5a52719a2ad8
Ray Becoskie <ray@becoskie.com>
2018-09-25 19:08:31 -0500

    Reset head try again

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Search from "./components/Search";
-import Wrapper from "./components/Wrapper";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
     <div>
-      <Navbar />
-      <Wrapper>
-      <Route exact path="/" component={Search} />
-      <Route exact path="/saved" component={Search} />
-      </Wrapper>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   </Router>
 );
