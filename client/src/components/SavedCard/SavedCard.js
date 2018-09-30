4c9c09129f2f82368764ba1bcf8df6fe3b38ce14
Ray Becoskie <ray@becoskie.com>
2018-09-30 04:51:31 -0500

    Added the saved and delete feature

---
+++ client/src/components/SavedCard/SavedCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./SavedCard.css";
+
+const SavedCard = props => {
+  const saveCard = props.saved.map(a => (
+    <div className="container col-12 mb-4" key={a._id}>
+      <div className="card">
+        <div className="card-body">
+          <a href={a.url} target="_blank" className="card-link">
+            <h5 className="card-title">{a.title}</h5>
+          </a>
+          <button type="submit" className="btn btn-danger" onClick={() => props.handleDelete(a._id)} title="Click to delete this article.">Delete</button>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return <div className="row">{saveCard}</div>;
+};
+
+export default SavedCard;
